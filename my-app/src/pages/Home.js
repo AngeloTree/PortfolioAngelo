e89988712f35d636b5e83ef297e7e31b364d0d65
Angelo Chaknis <angelo@Angelos-MacBook-Pro.local>
2022-11-26 16:05:27 -0700

    Added A Tags to social media links

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,48 +1,66 @@
 import React from "react";
-import TwitterIcon from '@mui/icons-material/Twitter';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import "../styles/Home.css";
 
 const Home = () => {
-    return ( 
-        <div className="home">
-            <div className="about">
-                <h2> Hi, My Name is Angelo</h2>
-                <div className="prompt"> 
-                    <p> 
-                    A software developer with a pasion for learning and creating.
-                    </p>
-                    <LinkedInIcon />
-                    <TwitterIcon />
-                    <GitHubIcon />
-                </div>
+  return (
+    <div className="home">
+      <div className="about">
+        <h2> Hi, My Name is Angelo</h2>
+        <div className="prompt">
+          <p>A software developer with a pasion for learning and creating.</p>
+          <div className="prompt-holder">
+            <div className="prompt-icon">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/angelo-chaknis-866857238/"
+              >
+                <LinkedInIcon />
+              </a>
             </div>
-            <div className="skills">
-                <h1> Skills </h1>
-                <ol className="list">
-                    <li className="item">
-                        <h2> Front-End </h2>
-                        <span>
-                            ReactJS, HTML, CSS, BootStrap, MaterialUI, NPM, Flexbox
-                        </span>
-                    </li>
-                    <li className="item">
-                        <h2> Back-End </h2>
-                        <span>
-                            NodeJS, MySQL, SQL, ExpressJS, AWS
-                        </span>
-                    </li>
-                    <li className="item">
-                        <h2> Languages </h2>
-                        <span>
-                            JavaScript, Python, Lua, C#
-                        </span>
-                    </li>
-                </ol>
+            <div className="prompt-icon">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/AngeloTree"
+              >
+                <GitHubIcon />
+              </a>
             </div>
+            <div className="prompt-icon">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://twitter.com/palmsci"
+              >
+                <TwitterIcon />
+              </a>
+            </div>
+          </div>
         </div>
-     );
-}
- 
+      </div>
+      <div className="skills">
+        <h1> Skills </h1>
+        <ol className="list">
+          <li className="item">
+            <h2> Front-End </h2>
+            <span>ReactJS, HTML, CSS, BootStrap, MaterialUI, NPM, Flexbox</span>
+          </li>
+          <li className="item">
+            <h2> Back-End </h2>
+            <span>NodeJS, MySQL, SQL, ExpressJS, AWS</span>
+          </li>
+          <li className="item">
+            <h2> Languages </h2>
+            <span>JavaScript, Python, Lua, C#</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
